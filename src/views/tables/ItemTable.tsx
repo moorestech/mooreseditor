da1438c2ab4a77a66af5877601711548e593e012
sakastudio <sakastudio100@gmail.com>
2022-08-13 23:34:53 +0900

    他のところをクリックしたら戻す処理

--- src/views/tables/ItemTable.tsx
+++ src/views/tables/ItemTable.tsx
@@ -1,5 +1,5 @@
 // ** React Imports
-import React, { useState } from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 
 // ** MUI Imports
 import Paper from '@mui/material/Paper'
@@ -23,11 +23,36 @@ const createData = (name: string, maxStacks: number) => {
 }
 
 const Row = (props: { row: ReturnType<typeof createData> }) => {
-  // ** Props
   const { row } = props
-
-  // ** State
   const [editName,setEditName] = useState<boolean>(false);
+  const [editMaxStacks,setMaxStacks] = useState<boolean>(false);
+
+  const insideRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    //対象の要素を取得
+    const el = insideRef.current;
+
+    //対象の要素がなければ何もしない
+    if (!el) return;
+
+    //クリックした時に実行する関数
+    const handleClickOutside = (e: MouseEvent) => {
+      if (el?.contains(e.target as Node)) {
+        setEditName(true)
+      } else {
+        setEditName(false)
+      }
+    };
+
+    //クリックイベントを設定
+    document.addEventListener("click", handleClickOutside);
+
+    //クリーンアップ関数
+    return () => {
+      //コンポーネントがアンマウント、再レンダリングされたときにクリックイベントを削除
+      document.removeEventListener("click", handleClickOutside);
+    }});
 
   return (
     <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
@@ -35,13 +60,20 @@ const Row = (props: { row: ReturnType<typeof createData> }) => {
         <img src='/images/logos/mastercard-label.png' alt='testItem' width={40} height={40} />
       </TableCell>
 
-      <TableCell component='th' scope='row'  onClick={() => setEditName(!editName)}>
-        {editName ?
-          <TextField fullWidth label='Name' placeholder='Item Name' size={"small"} defaultValue={row.name} autoFocus={true} /> :
-          <p>{row.name}</p>
+      <TableCell component='th' scope='row' ref={insideRef}>
+        {
+          editName ?
+            <TextField fullWidth label='Name' placeholder='Item Name' size={"small"} defaultValue={row.name} autoFocus={true} /> :
+            <p>{row.name}</p>
         }
       </TableCell>
-      <TableCell align='right'>{row.maxStacks}</TableCell>
+      <TableCell align='right'>
+        {
+          editMaxStacks ?
+            <TextField fullWidth label='MaxStacks' placeholder='row.maxStacks' size={"small"} defaultValue={row.maxStacks} autoFocus={true} type='number'  /> :
+            <p>{row.maxStacks}</p>
+        }
+      </TableCell>
       <TableCell />
     </TableRow>
   )
@@ -49,10 +81,6 @@ const Row = (props: { row: ReturnType<typeof createData> }) => {
 
 const rows = [
   createData('Frozen yoghurt', 159),
-  createData('Ice cream sandwich', 237),
-  createData('Eclair', 262),
-  createData('Cupcake', 305),
-  createData('Gingerbread', 356)
 ]
 
 function ItemTable() {

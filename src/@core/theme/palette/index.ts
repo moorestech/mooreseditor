f83b201a3007abd1af53c4b887c60345bab30dd3
sakastudio <sakastudio100@gmail.com>
2022-08-29 12:22:32 +0900

    テーマ色変更

--- src/@core/theme/palette/index.ts
+++ src/@core/theme/palette/index.ts
@@ -10,7 +10,7 @@ const DefaultPalette = (mode: PaletteMode, themeColor: ThemeColor) => {
 
   const primaryGradient = () => {
     if (themeColor === 'primary') {
-      return '#C6A7FE'
+      return '#ffc8a8'
     } else if (themeColor === 'secondary') {
       return '#9C9FA4'
     } else if (themeColor === 'success') {
@@ -36,9 +36,9 @@ const DefaultPalette = (mode: PaletteMode, themeColor: ThemeColor) => {
     },
     mode: mode,
     primary: {
-      light: '#9E69FD',
-      main: '#9155FD',
-      dark: '#804BDF',
+      light: '#fd9f69',
+      main: '#fc9356',
+      dark: '#de814b',
       contrastText: '#FFF'
     },
     secondary: {

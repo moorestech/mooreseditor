839d2c8ab11d4eb1329a36802a46835aed08fc67
sakastudio <sakastudio100@gmail.com>
2022-09-21 16:36:43 +0900

    ブロックのモーダル

--- src/views/tables/BlockTableRow.tsx
+++ src/views/tables/BlockTableRow.tsx
@@ -14,6 +14,12 @@ import {Select} from "@mui/material";
 import Button from "@mui/material/Button";
 import Pencil from 'mdi-material-ui/pencil';
 import IconButton from "@mui/material/IconButton";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import TextField from "@mui/material/TextField";
+import DialogActions from "@mui/material/DialogActions";
 
 interface ItemTableRowProps {
   name: string;
@@ -21,6 +27,17 @@ interface ItemTableRowProps {
 }
 
 const BlockTableRow = (props: { row: ItemTableRowProps }) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    console.log("aaa");
+    setOpen(false);
+  };
+
   return (
     <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
 
@@ -61,8 +78,31 @@ const BlockTableRow = (props: { row: ItemTableRowProps }) => {
       </TableCell>
 
       <TableCell>
-        <IconButton aria-label='expand row' size='small'>
+        <IconButton aria-label='expand row' size='small' onClick={handleClickOpen}>
           <Pencil/>
+
+          <Dialog open={open} onClose={handleClose}>
+            <DialogTitle>Subscribe</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To subscribe to this website, please enter your email address here. We
+                will send updates occasionally.
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Email Address"
+                type="email"
+                fullWidth
+                variant="standard"
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={handleClose}>Subscribe</Button>
+            </DialogActions>
+          </Dialog>
         </IconButton>
       </TableCell>
 

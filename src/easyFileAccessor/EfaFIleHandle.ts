2bbddcc0e493cb34be4adc9ff9e38bdd3b4ea747
sakastudio <sakastudio100@gmail.com>
2022-09-30 21:22:52 +0900

    ファイルの修正

--- src/easyFileAccessor/EfaFIleHandle.ts
+++ src/easyFileAccessor/EfaFIleHandle.ts
@@ -1,18 +1,19 @@
 import {EfaDirectoryHandle} from "./EfaDirectoryHandle";
 import {EfaHandle, FileSystemKind} from "./EfaHandle";
+import {FileWithDirectoryAndFileHandle} from "browser-fs-access";
 
 export class EfaFileHandle implements EfaHandle {
   readonly kind: FileSystemKind;
   readonly name: string;
   readonly path: string;
-    
+
   private readonly parentDirectory: EfaDirectoryHandle;
 
-  constructor(parentDirectory: EfaDirectoryHandle, name: string, path: string) {
+  constructor(parentDirectory: EfaDirectoryHandle,fileHandle : FileWithDirectoryAndFileHandle) {
     this.kind = 'file';
-    this.name = name;
+    this.name = fileHandle.name;
+    this.path = fileHandle.webkitRelativePath;
     this.parentDirectory = parentDirectory;
-    this.path = path;
   }
 
   getParent(): EfaDirectoryHandle | undefined {

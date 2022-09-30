92178fe99fdc267fb69005a2d60ee4e922ec6b73
sakastudio <sakastudio100@gmail.com>
2022-09-30 14:52:04 +0900

    ファイルの親ディレクトリ

--- src/easyFileAccessor/EfaFIleHandle.ts
+++ src/easyFileAccessor/EfaFIleHandle.ts
@@ -1,16 +1,21 @@
 import {EfaDirectoryHandle} from "./EfaDirectoryHandle";
 import {EfaHandle, FileSystemKind} from "./EfaHandle";
 
-export class EfaFileHandle implements EfaHandle{
-    readonly kind: FileSystemKind;
-    readonly name: string;
-    readonly parentDirectory: EfaDirectoryHandle;
-    readonly path: string;
+export class EfaFileHandle implements EfaHandle {
+  readonly kind: FileSystemKind;
+  readonly name: string;
+  readonly path: string;
+    
+  private readonly parentDirectory: EfaDirectoryHandle;
 
-    constructor(parentDirectory: EfaDirectoryHandle, name: string, path: string){
-        this.kind = 'file';
-        this.name = name;
-        this.parentDirectory = parentDirectory;
-        this.path = path;
-    }
+  constructor(parentDirectory: EfaDirectoryHandle, name: string, path: string) {
+    this.kind = 'file';
+    this.name = name;
+    this.parentDirectory = parentDirectory;
+    this.path = path;
+  }
+
+  getParent(): EfaDirectoryHandle | undefined {
+    return this.parentDirectory;
+  }
 }

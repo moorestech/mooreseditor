84546fd608044bac18db59eda051d6f9c801263a
sakastudio <sakastudio100@gmail.com>
2022-10-05 13:57:28 +0900

    ファイル書き込みようメソッド

--- src/easyFileAccessor/EfaFIleHandle.ts
+++ src/easyFileAccessor/EfaFIleHandle.ts
@@ -7,10 +7,20 @@ export class EfaFileHandle implements EfaHandle {
   readonly path: string;
   readonly parentDirectory: EfaDirectoryHandle;
 
+  private readonly fileHandle : FileSystemFileHandle;
+
   constructor(path : string,parentDirectory: EfaDirectoryHandle,fileHandle : FileSystemFileHandle) {
     this.kind = 'file';
     this.name = fileHandle.name;
     this.path = path;
     this.parentDirectory = parentDirectory;
+    this.fileHandle = fileHandle;
+  }
+
+  async getFile() : Promise<File> {
+    return await this.fileHandle.getFile();
+  }
+  async createWritable() : Promise<FileSystemWritableFileStream> {
+    return await this.fileHandle.createWritable();
   }
 }

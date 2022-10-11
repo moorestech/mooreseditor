d2cc72346b455abfe9b9535b773c5f07362ae3f5
sakastudio <sakastudio100@gmail.com>
2022-10-11 10:10:41 +0900

    ブロックのエレメントを作成

---
+++ src/mod/element/Block.ts
@@ -0,0 +1,21 @@
+import {Transform} from "./Transform";
+
+export class Block {
+  public readonly name: string;
+  public readonly type: string;
+  public readonly itemModId: string;
+  public readonly itemName: string;
+
+  public readonly modelTransform: Transform;
+  public readonly param : any;
+
+  constructor(name: string, type: string, itemModId: string, itemName: string, modelTransform: Transform,param : any) {
+    this.name = name;
+    this.type = type;
+    this.itemModId = itemModId;
+    this.itemName = itemName;
+    this.modelTransform = modelTransform;
+    this.param = param;
+  }
+
+}
---
+++ src/mod/element/Vector3.ts
@@ -0,0 +1,11 @@
+export class Vector3 {
+  public readonly x: number;
+  public readonly y: number;
+  public readonly z: number;
+
+  constructor(x: number, y: number, z: number) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+}

55ebe8247828f9f2ce64bebe6a82e3d940541bc4
sakastudio <sakastudio100@gmail.com>
2022-10-29 20:59:01 +0900

    アイコン変更

--- src/navigation/vertical/index.tsx
+++ src/navigation/vertical/index.tsx
@@ -1,17 +1,16 @@
-// ** Icon imports
 import Login from 'mdi-material-ui/Login'
 import Table from 'mdi-material-ui/Table'
 import CubeOutline from 'mdi-material-ui/CubeOutline'
 import HomeOutline from 'mdi-material-ui/HomeOutline'
 import FormatLetterCase from 'mdi-material-ui/FormatLetterCase'
-import AccountCogOutline from 'mdi-material-ui/AccountCogOutline'
 import CreditCardOutline from 'mdi-material-ui/CreditCardOutline'
 import AccountPlusOutline from 'mdi-material-ui/AccountPlusOutline'
 import AlertCircleOutline from 'mdi-material-ui/AlertCircleOutline'
 import GoogleCirclesExtended from 'mdi-material-ui/GoogleCirclesExtended'
-
-// ** Type import
+import Book from "mdi-material-ui/Book";
+import CogTransfer from "mdi-material-ui/CogTransfer";
 import { VerticalNavItemsType } from 'src/@core/layouts/types'
+import {DiamondStone, StateMachine} from "mdi-material-ui";
 
 const navigation = (): VerticalNavItemsType => {
   return [
@@ -25,32 +24,32 @@ const navigation = (): VerticalNavItemsType => {
     },
     {
       title: 'Item',
-      icon: HomeOutline, //画像はこうする icon: () => <img src="/images/favicon.png" alt={"aa"} width={24} height={24}/>,
+      icon: () => <img src="/images/icon/ingot_icon.png" alt={"item"} width={24} height={24}/>,
       path: '/config/item'
     },
     {
       title: 'Block',
-      icon: HomeOutline,
+      icon: CubeOutline,
       path: '/config/block'
     },
     {
       title: 'Ore',
-      icon: HomeOutline,
+      icon: DiamondStone,
       path: '/config/ore'
     },
     {
       title: 'Quest',
-      icon: HomeOutline,
+      icon: Book,
       path: '/config/quest'
     },
     {
       title: 'CraftRecipe',
-      icon: HomeOutline,
+      icon: () =>  <img src="/images/icon/craft_icon.png" alt={"craft recipe"} width={24} height={24}/>,
       path: '/config/craftRecipe'
     },
     {
       title: 'MachineRecipe',
-      icon: HomeOutline,
+      icon: StateMachine,
       path: '/config/machineRecipe'
     },
     {

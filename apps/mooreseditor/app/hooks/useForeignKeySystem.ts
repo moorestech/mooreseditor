1924fc963566999b9ac6f54a29d1be0ced36c050
sakastudio <sakastudio100@gmail.com>
2024-09-01 12:55:25 +0900

    乱数を追加するように

--- apps/mooreseditor/app/hooks/useForeignKeySystem.ts
+++ apps/mooreseditor/app/hooks/useForeignKeySystem.ts
@@ -11,8 +11,8 @@ export const useForeignKeySystem = (master: ReturnType<typeof useMasterDirectory
         const [schemaId, idPropName, labelPropName] = parseForeignKey(foreignKey)
 
         return (master.getEntries(schemaId))?.data.map((row: any) => ({
-            value: row[idPropName] ?? "value " + idPropName + "が未設定",
-            label: row[labelPropName] ?? row[idPropName] ?? "labelが未設定",
+            value: row[idPropName] ?? ("value " + idPropName + "が未設定 foreignKey "+foreignKey + Math.random()),
+            label: row[labelPropName] ?? row[idPropName] ?? "labelが未設定 foreignKey "+foreignKey,
         }))
     }
 

a26ffc3ce2e265b09ab0fb8e1468f6a42b56b2ae
sakastudio <katsumi.sato@aktsk.jp>
2025-06-21 18:29:15 +0900

    delete moores tech old

--- apps/mooreseditor_old/app/components/SchemaEditor/inputs/StringInput.tsx
+++
@@ -1,8 +0,0 @@
-import { TextInput } from "@mantine/core"
-import { ComponentProps } from "react"
-
-export const StringInput = (props: ComponentProps<typeof TextInput>) => {
-  return (
-    <TextInput {...props} />
-  )
-}
--- apps/mooreseditor_old/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
-
-export default defineConfig({
-    plugins: [remix(), netlifyPlugin(), tsconfigPaths()],
-});
